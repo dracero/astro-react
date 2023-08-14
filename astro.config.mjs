10ccaf42e68a36a1a14bb6d5ae9feaff2d63cf75
dracero <dracero>
2023-08-14 19:44:34 -0300

    router

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,15 +1,16 @@
 import { defineConfig } from 'astro/config';
 import react from "@astrojs/react";
-
 import vercel from "@astrojs/vercel/serverless";
+import svelte from "@astrojs/svelte";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [react()],
+  integrations: [react(), svelte()],
   output: 'server',
   // default is 'dist'
   bundler: 'esbuild' // default is 'esbuild'
   //
   ,
+
   adapter: vercel()
 });
---
+++ src/components/Alert.svelte
@@ -0,0 +1,8 @@
+<script>
+    function showAlert() {
+      alert('¡Hola!');
+    }
+  </script>
+  
+  <button on:click={showAlert}>Haz clic aquí</button>
+  
